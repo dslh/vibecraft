2043586f22669f4b94a7746ce4b513b22284fac7
Doug Hammond <d.lakehammond@gmail.com>
2026-03-14 18:23:05 +0100

    Debugging LAN games

--- harness/ports.py
+++ harness/ports.py
@@ -5,8 +5,9 @@ DEFAULT_BASE_PORT = 5100
 
 def make_portconfig(base_port: int, num_players: int) -> Portconfig:
     server_ports = [base_port, base_port + 1]
+    guests = num_players - 1
     player_ports = [
         [base_port + 2 + 2 * i, base_port + 3 + 2 * i]
-        for i in range(num_players)
+        for i in range(guests)
     ]
     return Portconfig(server_ports=server_ports, player_ports=player_ports)
